91e34554b87854f396723e5243b9db56727f7aac
rickster Muyco <rickslinger06@icloud.com>
2024-04-15 17:15:59 -0400

    push to main branch

---
+++ SpringCar/src/test/java/com/SpringCar/Services/CarServiceTest.java
@@ -0,0 +1,38 @@
+package com.SpringCar.Services;
+
+import com.SpringCar.Models.Car;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarServiceTest {
+
+    Car car1;
+    Car car2;
+    @BeforeEach
+    void setUp() {
+
+       car1 = new Car(1000,"model S","tesla","white");
+        car2 = new Car(1001,"civic","Honda","blue");
+
+    }
+
+    @Test
+    void getAllCars() {
+
+        List<Car> caList = new ArrayList<>();
+        caList.add(car1);
+        caList.add(car2);
+
+       assertEquals(2,caList.size());
+
+    }
+
+    @Test
+    void getCarsByUserId() {
+    }
+}
